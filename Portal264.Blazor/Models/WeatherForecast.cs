baf434dc5b3bcfb38ab9bb37df5467ae9e7ce2e9
shuntaro4 <shuntaro4@gmail.com>
2019-06-11 23:18:29 +0900

    add WeatherForecast.cs.

---
+++ Portal264.Blazor/Models/WeatherForecast.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Portal264.Blazor.Models
+{
+    public interface IWeatherForecast
+    {
+        DateTime Date { get; set; }
+        string Summary { get; set; }
+        int TemperatureC { get; set; }
+    }
+
+    public class WeatherForecast : IWeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public string Summary { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    }
+}
