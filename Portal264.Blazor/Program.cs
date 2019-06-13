74e4de5281e88a8ed4d2dd9d9534d4a39030b112
shuntaro4 <shuntaro4@gmail.com>
2019-06-13 21:50:28 +0900

    update Todo comment.

--- Portal264.Blazor/Program.cs
+++ Portal264.Blazor/Program.cs
@@ -2,6 +2,7 @@
 
 namespace Portal264.Blazor
 {
+    // Todo tutoral https://itnext.io/using-bing-maps-in-blazor-with-jsinterop-and-typescript-90e888e0e2fd
     public class Program
     {
         public static void Main(string[] args)
