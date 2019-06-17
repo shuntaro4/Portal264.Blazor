69c6f5ee3d8e8995bdc6033181b447cb7cc172e4
shuntaro4 <shuntaro4@gmail.com>
2019-06-17 23:07:16 +0900

    add RetrieveRealForecastAsync method.

--- Portal264.Blazor/Models/FetchDataModel.cs
+++ Portal264.Blazor/Models/FetchDataModel.cs
@@ -9,6 +9,10 @@ namespace Portal264.Blazor.Models
         IWeatherForecast[] WeatherForecasts { get; }
 
         Task RetrieveForecastsAsync();
+
+        WeatherDotGovForecast RealWeatherForecast { get; }
+
+        Task RetrieveRealForecastAsync();
     }
 
     public class FetchDataModel : IFetchDataModel
@@ -16,19 +20,31 @@ namespace Portal264.Blazor.Models
         private HttpClient _http;
 
         private IWeatherForecast[] _weatherForecasts;
+
         public IWeatherForecast[] WeatherForecasts
         {
             get => _weatherForecasts;
             private set => _weatherForecasts = value;
         }
 
+        private WeatherDotGovForecast _realWeatherForecast;
+
+        public WeatherDotGovForecast RealWeatherForecast { get => _realWeatherForecast; private set => _realWeatherForecast = value; }
+
         public FetchDataModel(HttpClient http)
         {
             _http = http;
         }
+
         public async Task RetrieveForecastsAsync()
         {
             _weatherForecasts = await _http.GetJsonAsync<WeatherForecast[]>("sample-data/weather.json");
+        }
+
+        public async Task RetrieveRealForecastAsync()
+        {
+            _realWeatherForecast = await _http.GetJsonAsync<WeatherDotGovForecast>
+              ("https://api.weather.gov/gridpoints/ALY/59,14/forecast");
         }
     }
 }
