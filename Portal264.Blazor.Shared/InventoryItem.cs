fac775b2048cb74460bdcc02e370ea124d91b6bb
shuntaro4 <shuntaro4@gmail.com>
2019-06-23 23:24:47 +0900

    add InventoryItem class.

---
+++ Portal264.Blazor.Shared/InventoryItem.cs
@@ -0,0 +1,9 @@
+﻿namespace Portal264.Blazor.Shared
+{
+    public class InventoryItem
+    {
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+        public decimal Price { get; set; }
+    }
+}
