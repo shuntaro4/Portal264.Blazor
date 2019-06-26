33d60c5e15f1997bdd2467086d08d68452dd1286
shuntaro4 <shuntaro4@gmail.com>
2019-06-26 22:20:52 +0900

    Remove Todo comment.

--- Portal264.Blazor.Client/Program.cs
+++ Portal264.Blazor.Client/Program.cs
@@ -2,7 +2,6 @@
 
 namespace Portal264.Blazor.Client
 {
-    // todo next https://itnext.io/working-with-the-view-in-blazor-mvvm-functions-vs-code-behind-vs-viewmodel-ed3508dba86a
     public class Program
     {
         public static void Main(string[] args)
