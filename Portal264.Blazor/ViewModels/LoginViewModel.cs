89fd75c7974d86f289463faede73f91c11fb7f77
shuntaro4 <shuntaro4@gmail.com>
2019-06-30 03:43:39 +0900

    add Login.razor page.

---
+++ Portal264.Blazor/ViewModels/LoginViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Portal264.Blazor.ViewModels
+{
+    public class LoginViewModel : ILoginViewModel
+    {
+        private string _mailAddress;
+
+        public string MailAddress
+        {
+            get => _mailAddress;
+            set => _mailAddress = value;
+        }
+
+        public LoginViewModel()
+        {
+        }
+
+        public async Task LoginAsync()
+        {
+            await Task.Delay(500);
+            Console.WriteLine($"MailAddress={MailAddress}");
+        }
+    }
+}
--- Portal264.Blazor/Views/Login.razor.cs
+++ Portal264.Blazor/Views/Login.razor.cs
@@ -1,8 +1,11 @@
 ﻿using Microsoft.AspNetCore.Components;
+using Portal264.Blazor.ViewModels;
 
 namespace Portal264.Blazor.Views
 {
     public class LoginBase : ComponentBase
     {
+        [Inject]
+        public ILoginViewModel ViewModel { get; set; }
     }
 }
