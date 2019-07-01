5bf4f85557dd3db86fa0560d7ea853fd510ca2e0
shuntaro4 <shuntaro4@gmail.com>
2019-07-01 22:17:27 +0900

    add IAuthenticateRepository interface.

---
+++ Portal264.Blazor.Firebase/IAuthenticateRepository.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+
+namespace Portal264.Blazor.Firebase
+{
+    public interface IAuthenticateRepository
+    {
+        Task<bool> LoginAsync(string mailAddress, string password);
+
+        Task Logout();
+    }
+}
