91d63acc6ec9a9402e576c18f2e13bf91926cf96
shuntaro4 <shuntaro4@gmail.com>
2019-07-01 23:24:42 +0900

    add AuthenticateFirebaseRepository class.

---
+++ Portal264.Blazor.Firebase/AuthenticateFirebaseRepository.cs
@@ -0,0 +1,29 @@
+﻿using Firebase.Auth;
+using Portal264.Blazor.Firebase.Common;
+using System;
+using System.Threading.Tasks;
+
+namespace Portal264.Blazor.Firebase
+{
+    public class AuthenticateFirebaseRepository : IAuthenticateRepository
+    {
+        public async Task<bool> LoginAsync(string mailAddress, string password)
+        {
+            try
+            {
+                var auth = new FirebaseAuthProvider(new FirebaseConfig(FirebaseToken.ApiKey));
+                var authLink = await auth.SignInWithEmailAndPasswordAsync(mailAddress, password);
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+        }
+
+        public Task Logout()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
