93ce7b9c96626eec49b922b98808c89f3a765b0f
shuntaro4 <shuntaro4@gmail.com>
2019-07-01 23:24:21 +0900

    add FirebaseConfig class.

---
+++ Portal264.Blazor.Firebase/Common/FirebaseConfig.cs
@@ -0,0 +1,19 @@
+﻿namespace Portal264.Blazor.Firebase.Common
+{
+    internal static class FirebaseToken
+    {
+        internal static string ApiKey { get; } = "AIzaSyDtDlVpIapO8JVEI8vNchHJrj4M7TeG380";
+
+        internal static string AuthDomain { get; } = "portal264-1263a.firebaseapp.com";
+
+        internal static string DatabaseURL { get; } = "https://portal264-1263a.firebaseio.com";
+
+        internal static string ProjectId { get; } = "portal264-1263a";
+
+        internal static string StrageBucket { get; } = "";
+
+        internal static string MessagingSenderId { get; } = "606193601482";
+
+        internal static string AppId { get; } = "1:606193601482:web:558cad26072afd91";
+    }
+}
