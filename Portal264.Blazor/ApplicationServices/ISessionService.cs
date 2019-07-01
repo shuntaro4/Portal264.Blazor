7f9d495a2f0918aad59c94cc1daaf84fc0c4d5fb
shuntaro4 <shuntaro4@gmail.com>
2019-07-01 23:25:15 +0900

    add ISessionService interface.

---
+++ Portal264.Blazor/ApplicationServices/ISessionService.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+
+namespace Portal264.Blazor.ApplicationServices
+{
+    public interface ISessionService
+    {
+        Task<bool> LoginAsync(string mailAddress, string password);
+
+        Task LogoutAsync();
+    }
+}
