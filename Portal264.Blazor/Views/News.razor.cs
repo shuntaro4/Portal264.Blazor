338ffd745e41491b5916ba0fe22eb536d2ac3aa6
shuntaro4 <shuntaro4@gmail.com>
2019-07-03 00:46:53 +0900

    add news page.

---
+++ Portal264.Blazor/Views/News.razor.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Components;
+using Portal264.Blazor.ViewModels;
+
+namespace Portal264.Blazor.Views
+{
+    public class NewsBase : ComponentBase
+    {
+        [Inject]
+        public INewsViewModel ViewModel { get; set; }
+    }
+}
