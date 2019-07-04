541e5696ba9ee22453c143186ed85fc833ad1c60
shuntaro4 <shuntaro4@gmail.com>
2019-07-04 23:29:08 +0900

    add news domain model.

---
+++ Portal264.Blazor/Domain/News.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Portal264.Blazor.Domain
+{
+    public class News
+    {
+        public string Title { get; private set; }
+
+        private DateTime _startAt;
+
+        public string DisplayStartAt => _startAt.ToString("yyyy-MM-dd");
+
+        private DateTime _createdAt;
+
+        public string DisplayCreatedAt => _createdAt.ToString("yyyy-MM-dd");
+
+        public News(string title, DateTime startAt, DateTime createdAt)
+        {
+            Title = title;
+            _startAt = startAt;
+            _createdAt = createdAt;
+        }
+    }
+}
