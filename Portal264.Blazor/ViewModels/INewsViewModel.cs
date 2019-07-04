3683d925ee56f8e21e9178a24f9c7a0fe95feb58
shuntaro4 <shuntaro4@gmail.com>
2019-07-04 23:29:40 +0900

    add news collection to NewsViewModel.

--- Portal264.Blazor/ViewModels/INewsViewModel.cs
+++ Portal264.Blazor/ViewModels/INewsViewModel.cs
@@ -1,6 +1,10 @@
-﻿namespace Portal264.Blazor.ViewModels
+﻿using Portal264.Blazor.Domain;
+using System.Collections.ObjectModel;
+
+namespace Portal264.Blazor.ViewModels
 {
     public interface INewsViewModel
     {
+        ObservableCollection<News> News { get; }
     }
 }
--- Portal264.Blazor/ViewModels/NewsViewModel.cs
+++ Portal264.Blazor/ViewModels/NewsViewModel.cs
@@ -1,6 +1,21 @@
-﻿namespace Portal264.Blazor.ViewModels
+﻿using Portal264.Blazor.Domain;
+using System.Collections.ObjectModel;
+
+namespace Portal264.Blazor.ViewModels
 {
     public class NewsViewModel : INewsViewModel
     {
+        private ObservableCollection<News> _news;
+
+        public ObservableCollection<News> News
+        {
+            get => _news;
+            set => _news = value;
+        }
+
+        public NewsViewModel()
+        {
+            News = new ObservableCollection<News>();
+        }
     }
 }
