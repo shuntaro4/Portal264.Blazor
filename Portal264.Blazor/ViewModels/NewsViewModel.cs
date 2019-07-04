6e1f1ba4e830146431ba42b52df81bec6db9df76
shuntaro4 <shuntaro4@gmail.com>
2019-07-05 01:02:13 +0900

    add news list to News.razor.

--- Portal264.Blazor/ViewModels/NewsViewModel.cs
+++ Portal264.Blazor/ViewModels/NewsViewModel.cs
@@ -1,4 +1,5 @@
 ﻿using Portal264.Blazor.Domain;
+using System;
 using System.Collections.ObjectModel;
 
 namespace Portal264.Blazor.ViewModels
@@ -15,7 +16,11 @@ namespace Portal264.Blazor.ViewModels
 
         public NewsViewModel()
         {
-            News = new ObservableCollection<News>();
+            News = new ObservableCollection<News>()
+            {
+                new News("テスト１", new DateTime(2000,12,1), DateTime.Now),
+                new News("テスト２", new DateTime(2012,12,12), DateTime.Now.AddDays(-1))
+            };
         }
     }
 }
