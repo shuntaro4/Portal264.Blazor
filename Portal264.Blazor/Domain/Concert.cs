3ceced5c33aa7e824b7a7b4357f35bcfe6c33549
shuntaro4 <shuntaro4@gmail.com>
2019-07-06 22:50:01 +0900

    add Concert domain model.

---
+++ Portal264.Blazor/Domain/Concert.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Portal264.Blazor.Domain
+{
+    public class Concert
+    {
+        public string Title { get; private set; }
+
+        private DateTime _openAt;
+
+        public string DisplayDateOpenAt => _openAt.ToString("yyyy-MM-dd(ddd)");
+
+        public string DisplayTimeOpenAt => _openAt.ToString("HH:mm");
+
+        private DateTime _startAt;
+
+        public string DisplayDateStartAt => _startAt.ToString("yyyy-MM-dd(ddd)");
+
+        public string DisplayTimeStartAt => _startAt.ToString("HH:mm");
+
+        public bool Active { get; private set; }
+
+        public string Place { get; private set; }
+
+        public Concert(string title, DateTime openAt, DateTime startAt, bool active, string place)
+        {
+            Title = title;
+            _openAt = openAt;
+            _startAt = startAt;
+            Active = active;
+            Place = place;
+        }
+    }
+}
