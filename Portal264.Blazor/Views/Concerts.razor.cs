bc658e213a12c435a1e965239586c90ffbd45f49
shuntaro4 <shuntaro4@gmail.com>
2019-07-06 22:50:51 +0900

    add Concerts.razor.

---
+++ Portal264.Blazor/Views/Concerts.razor.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Components;
+using Portal264.Blazor.ViewModels;
+
+namespace Portal264.Blazor.Views
+{
+    public class ConcertsBase : ComponentBase
+    {
+        [Inject]
+        public IConcertsViewModel ViewModel { get; set; }
+    }
+}
