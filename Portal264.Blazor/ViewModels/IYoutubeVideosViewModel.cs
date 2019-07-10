f9932353525d2af647412a5ad947cda65b80d697
shuntaro4 <shuntaro4@gmail.com>
2019-07-10 22:56:41 +0900

    add YoutubeVideosViewModel.

---
+++ Portal264.Blazor/ViewModels/IYoutubeVideosViewModel.cs
@@ -0,0 +1,10 @@
+﻿using Portal264.Blazor.Domain;
+using System.Collections.ObjectModel;
+
+namespace Portal264.Blazor.ViewModels
+{
+    public interface IYoutubeVideosViewModel
+    {
+        ObservableCollection<YoutubeVideo> Videos { get; }
+    }
+}
---
+++ Portal264.Blazor/ViewModels/YoutubeVideosViewModel.cs
@@ -0,0 +1,28 @@
+﻿using Portal264.Blazor.Domain;
+using System;
+using System.Collections.ObjectModel;
+
+namespace Portal264.Blazor.ViewModels
+{
+    public class YoutubeVideosViewModel : IYoutubeVideosViewModel
+    {
+        private ObservableCollection<YoutubeVideo> _videos;
+
+        public ObservableCollection<YoutubeVideo> Videos
+        {
+            get => _videos;
+            set => _videos = value;
+        }
+
+        public YoutubeVideosViewModel()
+        {
+            // Videos = = new ObservableCollection<YoutubeVideo>();
+            Videos = new ObservableCollection<YoutubeVideo>()
+            {
+                new YoutubeVideo("JGBLgWBXn_A", "テスト１", new DateTime(2000,12,1)),
+                new YoutubeVideo("eyOhqCLLfKU", "テスト２", new DateTime(2000,12,2)),
+                new YoutubeVideo("3aPCIDabY30", "テスト３", new DateTime(2000,12,3)),
+            };
+        }
+    }
+}
