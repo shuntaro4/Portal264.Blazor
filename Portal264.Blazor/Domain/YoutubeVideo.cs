c28e1833822ce5ea466214fcb16ee14b199759c5
shuntaro4 <shuntaro4@gmail.com>
2019-07-10 22:37:11 +0900

    add Youtube video Model.

---
+++ Portal264.Blazor/Domain/YoutubeVideo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Portal264.Blazor.Domain
+{
+    public class YoutubeVideo
+    {
+        public int Id { get; private set; }
+
+        public string Title { get; private set; }
+
+        private DateTime _publishedAt;
+
+        public string DisplayDatePublishedAt => _publishedAt.ToString("yyyy-MM-dd(ddd)");
+
+        public string Url => "https://www.youtube.com/watch?v=" + Id;
+
+        public string ThumbnailUrl => "https://img.youtube.com/vi/" + Id + "/mqdefault.jpg";
+
+        public YoutubeVideo(int id, string title, DateTime publishedAt)
+        {
+            Id = id;
+            Title = title;
+            _publishedAt = publishedAt;
+        }
+    }
+}
