aa9809fccd5f20a2c6b6d68a43dc85532963ad5b
shuntaro4 <shuntaro4@gmail.com>
2019-07-13 00:04:28 +0900

    add GetVideos method to IYoutubeVideoService.

--- Portal264.Blazor/ApplicationServices/IYoutubeVideoService.cs
+++ Portal264.Blazor/ApplicationServices/IYoutubeVideoService.cs
@@ -1,1 +1,11 @@
+using Portal264.Blazor.Domain;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
+namespace Portal264.Blazor.ApplicationServices
+{
+    public interface IYoutubeVideoService
+    {
+        Task<List<YoutubeVideo>> GetVideosAsync();
+    }
+}
