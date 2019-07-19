20f3e64cf059224ba60ce3136376bcfc3c6bc124
shuntaro4 <shuntaro4@gmail.com>
2019-07-19 19:22:08 +0900

    add ConcertId to News model.

--- Portal264.Blazor/Domain/News.cs
+++ Portal264.Blazor/Domain/News.cs
@@ -4,6 +4,8 @@ namespace Portal264.Blazor.Domain
 {
     public class News
     {
+        public int ConcertId { get; private set; }
+
         public string Title { get; private set; }
 
         private DateTime _startAt;
@@ -14,8 +16,9 @@ namespace Portal264.Blazor.Domain
 
         public string DisplayCreatedAt => _createdAt.ToString("yyyy-MM-dd");
 
-        public News(string title, DateTime startAt, DateTime createdAt)
+        public News(int concertId, string title, DateTime startAt, DateTime createdAt)
         {
+            ConcertId = concertId;
             Title = title;
             _startAt = startAt;
             _createdAt = createdAt;
