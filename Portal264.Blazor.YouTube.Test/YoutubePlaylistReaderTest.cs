8667b0c12472f92915d9c476c6307ba760ea511e
shuntaro4 <shuntaro4@gmail.com>
2019-07-27 20:29:44 +0900

    add Test Project.

---
+++ Portal264.Blazor.YouTube.Test/YoutubePlaylistReaderTest.cs
@@ -0,0 +1,13 @@
+using Xunit;
+
+namespace Portal264.Blazor.YouTube.Test
+{
+    public class YoutubePlaylistReaderTest
+    {
+        [Fact(DisplayName = "Success to get the youtube playlist.")]
+        [Trait("YouTubePlaylistReader", "GetAsync")]
+        public void GetAsyncTrue1()
+        {
+        }
+    }
+}
