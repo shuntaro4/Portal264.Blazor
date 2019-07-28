6ac7f35f51b4634bd45c4ad995ca4b63cbff5595
shuntaro4 <shuntaro4@gmail.com>
2019-07-28 22:47:41 +0900

    add a YoutubePlaylistReader.GetAsync test.

--- Portal264.Blazor.YouTube.Test/YoutubePlaylistReaderTest.cs
+++ Portal264.Blazor.YouTube.Test/YoutubePlaylistReaderTest.cs
@@ -1,3 +1,8 @@
+using RichardSzalay.MockHttp;
+using System;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
 using Xunit;
 
 namespace Portal264.Blazor.YouTube.Test
@@ -6,8 +11,53 @@ namespace Portal264.Blazor.YouTube.Test
     {
         [Fact(DisplayName = "Success to get the youtube playlist.")]
         [Trait("YouTubePlaylistReader", "GetAsync")]
-        public void GetAsyncTrue1()
+        public async Task GetAsyncTrue1()
         {
+            var apiKey = "hoge";
+            var playlistId = "fuga";
+            var maxResults = 50;
+
+            using (var mockHttpClient = new MockHttpMessageHandler())
+            {
+                var urlBuilder = new StringBuilder();
+                urlBuilder.Append("https://www.googleapis.com/youtube/v3/playlistItems");
+                urlBuilder.Append("?part=snippet");
+                urlBuilder.Append("&playlistId=" + playlistId);
+                urlBuilder.Append("&maxResults=" + maxResults);
+                urlBuilder.Append("&key=" + apiKey);
+
+                var json = ReadJsonFile("playlist.json");
+                mockHttpClient.When(urlBuilder.ToString()).Respond("application/json", json);
+
+                var target = new YouTubePlaylistReader(apiKey, mockHttpClient.ToHttpClient())
+                {
+                    PlaylistId = playlistId,
+                    MaxResults = maxResults
+                };
+
+                var actual = await target.GetAsync();
+
+                Assert.NotNull(actual);
+                Assert.Equal(11, actual.items.Count);
+
+                var snipet = actual.items[0].snippet;
+                Assert.NotNull(snipet);
+
+                Assert.Equal("Song forÅc(HY cover)", snipet.title);
+                Assert.Equal(DateTime.Parse("2018-05-06T13:50:34.000Z").ToUniversalTime(), snipet.publishedAt.ToUniversalTime());
+                Assert.NotNull(snipet.resourceId);
+                Assert.Equal("JGBLgWBXn_A", snipet.resourceId.videoId);
+            }
+        }
+
+        private string ReadJsonFile(string fileName)
+        {
+            var path = @$"{Environment.CurrentDirectory}\TestResources\Json\{fileName}";
+            using (var stream = new FileStream(path, FileMode.Open))
+            using (var reader = new StreamReader(stream, Encoding.UTF8))
+            {
+                return reader.ReadToEnd();
+            }
         }
     }
 }
