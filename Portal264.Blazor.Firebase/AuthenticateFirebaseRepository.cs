74d4a5b5f152535ca62b3bd0734ad6bf6e9523b5
shuntaro4 <shuntaro4@gmail.com>
2019-07-31 23:14:09 +0900

    remove "FirebaseAuthentication.net" nuget package.

--- Portal264.Blazor.Firebase/AuthenticateFirebaseRepository.cs
+++ Portal264.Blazor.Firebase/AuthenticateFirebaseRepository.cs
@@ -1,26 +1,13 @@
-﻿using Firebase.Auth;
-using Portal264.Blazor.Firebase.Common;
-using System;
+﻿using System;
 using System.Threading.Tasks;
 
 namespace Portal264.Blazor.Firebase
 {
     public class AuthenticateFirebaseRepository : IAuthenticateRepository
     {
-        private FirebaseAuthLink _authLink;
-
         public async Task<bool> LoginAsync(string mailAddress, string password)
         {
-            try
-            {
-                var auth = new FirebaseAuthProvider(new FirebaseConfig(FirebaseToken.ApiKey));
-                _authLink = await auth.SignInWithEmailAndPasswordAsync(mailAddress, password);
-                return true;
-            }
-            catch (Exception)
-            {
-                return false;
-            }
+            throw new NotImplementedException();
         }
 
         public Task LogoutAsync()
